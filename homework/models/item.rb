189463a82e596b7ac792bf933fd1d7538c5480fa
renvelvet <reshapuspita734@gmail.com>
2021-07-31 16:26:58 +0700

    fix params prolem

--- homework/models/item.rb
+++ homework/models/item.rb
@@ -2,32 +2,43 @@ require_relative '../db/mysql_connector'
 require_relative './category'
 
 class Item
-  attr_accessor :id, :name, :price, :category
+  attr_accessor :id, :name, :price, :categories
 
-  def initialize(name, price, id, category = nil)
-    @id = id
-    @name = name
-    @price = price
-    @category = category
+  def initialize(params)
+    @id = params[:id]
+    @name = params[:name]
+    @price = params[:price]
+    @categories = []
   end
 
   def self.find_all
     client = create_db_client
     raw_data = client.query("select * from items")
-    items = Array.new
   
     convert_sql_result_to_array(raw_data)
   end
 
   def self.find_by_id(id)
     client = create_db_client
+    raw_data = client.query("select *
+    from items
+    where items.id = #{id}")
+   
+    p convert_sql_result_to_array(raw_data).first 
+    convert_sql_result_to_array(raw_data).first
+  end
+
+  def self.find_with_categories(id)
+    client = create_db_client
     raw_data = client.query("select items.id, items.name, items.price, categories.name as 'category_name', categories.id as 'category_id'
     from items
     join item_categories on items.id = item_categories.item_id
     join categories on item_categories.category_id = categories.id 
     where items.id = #{id}")
-   
-    convert_sql_result_to_array(raw_data).first
+
+    item = find_by_id(id)
+    item.categories = Category.find_by_id(raw_data.first['category_id'])
+    item
   end
 
   def valid?
@@ -39,16 +50,10 @@ class Item
     items = Array.new
 
     result.each do |row|
-      category = Category.new({
-        name: row['category_name'],
-        id: row['category_id']
-      })
-
       item = Item.new({
         id: row['id'],
         name:row['name'],
-        price: row['price'],
-        category: category
+        price: row['price']
       })
 
       items << item
