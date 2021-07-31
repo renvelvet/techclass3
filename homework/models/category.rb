4989e59d3bbc1beaa0e892ce228c0144a03e89f6
renvelvet <reshapuspita734@gmail.com>
2021-07-31 16:27:26 +0700

    add find_all, find_by_id method

---
+++ homework/models/category.rb
@@ -0,0 +1,40 @@
+class Category
+    attr_accessor :name, :id
+  
+    def initialize(params)
+      @name = params[:name]
+      @id = params[:id]
+    end
+
+    def self.find_all
+      client = create_db_client
+      raw_data = client.query("select * from categories")
+
+      convert_sql_result_to_array(raw_data)
+    end
+
+    def self.find_by_id(id)
+      client = create_db_client
+      raw_data = client.query("select *
+      from categories
+      where categories.id = #{id}")
+     
+      convert_sql_result_to_array(raw_data).first
+    end
+
+    def self.convert_sql_result_to_array(result)
+      categories = Array.new
+  
+      result.each do |row|
+        category = Category.new({
+          name:row['name'],
+          id: row['id']
+        })
+  
+        categories << category
+      end
+  
+      categories
+    end
+  end
+  
